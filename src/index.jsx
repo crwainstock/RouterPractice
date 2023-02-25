916f5eb9d4d167d06644998110e207aa714c0aa6
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-02-25 18:38:14 +0100

    reconfigure index.html

--- src/index.jsx
+++ src/index.jsx
@@ -18,12 +18,4 @@ function App() {
   );
 }
 
-function Home() {
-  return <h1>Hello, React Router!</h1>;
-}
-
-function About() {
-  return <h1>About page goes here! 🎉</h1>;
-}
-
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
