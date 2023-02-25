5ef2cc370935a40a8411fb60715dab2acaa81247
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-02-25 18:41:46 +0100

    update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -7,10 +7,14 @@ import About from "./pages/About";
 function App() {
   return (
     <BrowserRouter>
-      <nav className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </nav>
+      <header>
+        <Link className="site-logo" to="/">
+          #VanLife
+        </Link>
+        <nav>
+          <Link to="/about">About</Link>
+        </nav>
+      </header>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
