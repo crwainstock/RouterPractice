387cd408f7dc1a1749d2fb4e115cf1a28bcf85c5
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-02-26 15:30:23 +0100

    build van detail page

--- src/pages/VanList.jsx
+++ src/pages/VanList.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import "../index.css";
 
 export default function VanList() {
@@ -18,17 +19,19 @@ export default function VanList() {
   // };
 
   const vanElements = vans.map((van) => (
-    <div key={van.id} className="van-tile">
-      <img src={van.imageUrl} />
-      <div className="van-info">
-        <h3>{van.name}</h3>
-        <p>
-          ${van.price}
-          <span>/day</span>
-        </p>
+    <Link to={`/vans/${van.id}`}>
+      <div key={van.id} className="van-tile">
+        <img src={van.imageUrl} />
+        <div className="van-info">
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
+        </div>
+        <i className={`van-type ${van.type} selected`}>{van.type}</i>
       </div>
-      <i className={`van-type ${van.type} selected`}>{van.type}</i>
-    </div>
+    </Link>
   ));
 
   return (
