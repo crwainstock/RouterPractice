e4ade779a06bbfe9d2db5b552069ab6b0c232775
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-02-26 12:53:26 +0100

    added useEffect hook & rendered van data

--- src/pages/VanList.jsx
+++ src/pages/VanList.jsx
@@ -1,10 +1,35 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function VanList() {
-  return (
-    <div>
-      <h1>Vans page goes here 🚐</h1>
+  const [vans, setVans] = useState([]);
+
+  useEffect(() => {
+    fetch("/api/vans")
+      .then((res) => res.json())
+      .then((data) => setVans(data.vans));
+  }, []);
+
+  // const getVans = async () => {
+  //   let results = await fetch(`/api/vans`);
+  //   let data = results.json();
+  //   setVans(data);
+  // };
+
+  const vanElements = vans.map((van) => (
+    <div key={van.id} className="van-tile">
+      <img src={van.imageUrl} />
+      <div className="van-info">
+        <h3>{van.name}</h3>
+        <p>
+          ${van.price}
+          <span>/day</span>
+        </p>
+      </div>
+      <i className={`van-type ${van.type} selected`}>{van.type}</i>
     </div>
-  );
+  ));
+
+  return <div>{vanElements}</div>;
 }
