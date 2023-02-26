f97df9ad2c8e94ee3482eb90ec4a77c8d1923608
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-02-26 12:24:37 +0100

    add h1 in vanlist

--- src/pages/VanList.js
+++ src/pages/VanList.js
@@ -2,5 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function VanList() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Vans page goes here 🚐</h1>
+    </div>
+  );
 }
