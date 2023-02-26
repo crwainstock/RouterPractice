1d46a1ddcefe922e23d357ae09f158f7472e6251
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-02-26 13:02:03 +0100

    added margin on bottom of vanlist component

--- src/pages/VanList.jsx
+++ src/pages/VanList.jsx
@@ -33,6 +33,7 @@ export default function VanList() {
 
   return (
     <div className="van-list-container">
+      <h1>Explore our van options</h1>
       <div className="van-list">{vanElements}</div>
     </div>
   );
