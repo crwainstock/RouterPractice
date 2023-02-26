b582e402f3eb0f0444c87c4e4f22141e2ac3d45f
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-02-26 12:26:20 +0100

    import vanlist in index

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import VanList from "./pages/VanList";
 import "../src/index.css";
 import "./server";
 
