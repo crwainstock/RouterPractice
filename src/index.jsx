8a8df938ae2108f00d354ef46139c82c1f2e1bf1
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-03-01 20:20:21 +0100

    create header and layout components

--- src/index.jsx
+++ src/index.jsx
@@ -5,26 +5,20 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import VanList from "./pages/VanList";
 import VanDetail from "./pages/VanDetail";
+import Layout from "./components/Layout";
 import "../src/index.css";
 import "./server";
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">
-          #VanLife
-        </Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/vans" element={<VanList />} />
-        <Route path="/vans/:id" element={<VanDetail />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/vans" element={<VanList />} />
+          <Route path="/vans/:id" element={<VanDetail />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
