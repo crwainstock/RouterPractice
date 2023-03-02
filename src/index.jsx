e05a7ee04f83839d1c6389a0739288e410b4116e
Crystal Rose-Wainstock <crwainstock@gmail.com>
2023-03-02 11:21:43 +0100

    update relative paths

--- src/index.jsx
+++ src/index.jsx
@@ -19,12 +19,13 @@ function App() {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/vans" element={<VanList />} />
-          <Route path="/vans/:id" element={<VanDetail />} />
-          <Route path="/host" element={<HostLayout />}>
-            <Route path="/host/income" element={<Income />} />
-            <Route path="/host/reviews" element={<Reviews />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<VanList />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="host" element={<HostLayout />}>
+            <Route path="host" element={<Dashboard />} />
+            <Route path="income" element={<Income />} />
+            <Route path="reviews" element={<Reviews />} />
           </Route>
         </Route>
       </Routes>
