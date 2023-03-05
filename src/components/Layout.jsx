53a04a8ef5a84bca2b39ab167e36bb7d3ecd6a6a
Crystal <crwainstock@gmail.com>
2023-03-05 13:55:17 +0100

    added footer, styling on host page still a bit messed up

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 import { Outlet } from "react-router-dom";
 
 export default function Layout() {
   return (
-    <div>
+    <div className="site-wrapper">
       <Header />
-      <Outlet />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
     </div>
   );
 }
