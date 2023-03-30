6680305f0caff223fef9b15534babc9c7ad812f1
Crystal <crwainstock@gmail.com>
2023-03-30 09:57:38 +0200

    updated route to index in index.jsx, but relative links still not working like they say they should in the tutorial

--- src/index.jsx
+++ src/index.jsx
@@ -19,11 +19,12 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="vans" element={<VanList />} />
           <Route path="vans/:id" element={<VanDetail />} />
+
           <Route path="host" element={<HostLayout />}>
             <Route index element={<Dashboard />} />
             <Route path="income" element={<Income />} />
