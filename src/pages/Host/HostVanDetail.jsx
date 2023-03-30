df61076d9d8cb75c396e0bd037e466a2941ea3d3
Crystal <crwainstock@gmail.com>
2023-03-30 09:09:10 +0200

    built out host vans list page, styling, & sample server with host ids

--- src/pages/Host/HostVanDetail.jsx
+++ src/pages/Host/HostVanDetail.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function HostVanDetail() {
   return (
     <div>
-      <h1>Host vans info goes here.</h1>
+      <h1>Host vans details go here.</h1>
     </div>
   );
 }
