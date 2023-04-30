b3ce2f63d7b7a2138ae23e9d65faac67b1b1f307
Crystal <crwainstock@gmail.com>
2023-04-30 14:17:33 +0200

    add more detailed message to the error.jsx file, useRouteError hook

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,5 +1,18 @@
 import React from "react";
+import { useRouteError } from "react-router-dom";
+
+//The content of the error message here is produced in the api.js file, fetch request. If the fetch isn't
+//successful, the error will throw.
 
 export default function Error() {
-  return <h1>An error occured!</h1>;
+  const error = useRouteError();
+  console.log(error);
+  return (
+    <div>
+      <h1>Error: {error.message}</h1>
+      <pre>
+        {error.status} - {error.statusText}
+      </pre>
+    </div>
+  );
 }
