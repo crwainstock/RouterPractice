0e5dccbddf6eb6a6aaba0d82c4391f37b5f6a583
Crystal <crwainstock@gmail.com>
2023-05-20 12:36:47 +0200

    idk. still getting error with redirect...tried using useNavigate, but no luck.:

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,26 @@
-import { redirect } from "react-router-dom";
+// import { redirect } from "react-router-dom";
+
+// export async function requireAuth() {
+//   const isLoggedIn = false;
+
+//   // Something isn't working with the redirect here...manually going to the /login page works, but
+//   // an error comes up instead of redirecting when navigating to the host page while isLoggedIn is false
+//   // Return instead of throw doesn't work -- just accesses the host page
+//   if (!isLoggedIn) {
+//     throw redirect("/login");
+//   }
+// }
+
+import { useNavigate } from "react-router-dom";
 
 export async function requireAuth() {
   const isLoggedIn = false;
+  const navigate = useNavigate();
 
-  // Something isn't working with the redirect here...manually going to the /login page works, but
-  // an error comes up instead of redirecting when navigating to the host page while isLoggedIn is false
   if (!isLoggedIn) {
-    throw redirect("/login");
+    navigate("/login");
+    return;
   }
+
+  // Continue with the authenticated logic
 }
