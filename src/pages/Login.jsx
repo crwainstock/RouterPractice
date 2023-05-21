5fbae8fec1d3d2634a8025e8fb5db788cbce52a5
Crystal <crwainstock@gmail.com>
2023-05-21 12:50:56 +0200

    added loader to login component, rendering you must log in first when accessing host pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { useLoaderData } from "react-router-dom";
 
-// export function loader({ request }) {
-//   return new URL(request.url).searchParams.get("message");
-// }
+export function loginLoader({ request }) {
+  return new URL(request.url).searchParams.get("message");
+}
 
 export default function Login() {
   const [loginFormData, setLoginFormData] = React.useState({
     email: "",
     password: "",
   });
-  // const message = useLoaderData();
+  const message = useLoaderData();
+  console.log(message);
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -28,7 +29,7 @@ export default function Login() {
   return (
     <div className="login-container">
       <h1>Sign in to your account</h1>
-      {/* {message && <h2 className="red">{message}</h2>} */}
+      {message && <h2 className="red">{message}</h2>}
       <form onSubmit={handleSubmit} className="login-form">
         <input
           name="email"
