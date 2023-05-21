fbc62745f2145ea07467e03b451de6a16c73154f
Crystal <crwainstock@gmail.com>
2023-05-21 14:12:03 +0200

    message from url login component is undefined, not sure why. it was working before

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,13 +5,13 @@ import { useLoaderData, useNavigate, Form } from "react-router-dom";
 import { loginUser } from "../api";
 
 // This function accesses the message in the URL from the utils function -- getting error
-// export function loginLoader({ request }) {
-//   if (request.url) {
-//     return new URL(request.url).searchParams.get("message");
-//   } else {
-//     return null;
-//   }
-// }
+export function loginLoader({ request }) {
+  if (request.url) {
+    return new URL(request.url).searchParams.get("message");
+  } else {
+    return null;
+  }
+}
 
 export async function action() {
   console.log("Action function");
@@ -22,13 +22,13 @@ export default function Login() {
   const [status, setStatus] = useState("idle");
   const [error, setError] = useState(null);
 
-  // const message = useLoaderData();
-  // console.log(message);
+  const message = useLoaderData();
+  console.log(message); //undefined???
 
   return (
     <div className="login-container">
       <h1>Sign in to your account</h1>
-      {/* {message && <h3 className="red">{message}</h3>} */}
+      {message && <h3 className="red">{message}</h3>}
       {error && <h3 className="red">{error.message}</h3>}
       <Form method="post" className="login-form">
         <input name="email" type="email" placeholder="Email address" />
