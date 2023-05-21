6b3afb082ff1b5f426e89f19efc0f5f885495a12
Crystal <crwainstock@gmail.com>
2023-05-21 19:28:16 +0200

    error gone, but login page redirect not quite working as intended

--- src/utils.js
+++ src/utils.js
@@ -6,10 +6,13 @@ export async function requireAuth() {
   if (!isLoggedIn) {
     throw redirect("/login?message=You must log in first.");
   } else {
-    return null;
+    throw redirect("/login");
   }
 }
 
 // The ?message=You must log in first allows us to send this message through the URL to render in the login page
 // if someone tries to access the host pages without being logged in. With useNavigate, you can send state through
 // the navigate bit, but not with redirect
+
+// This redirect isn't quite working how it should, but the error is not happening now that I added the second redirect
+// to the login page.
