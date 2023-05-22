3a680884942b4ecbf36ec73544132189ea964571
Crystal <crwainstock@gmail.com>
2023-05-22 09:54:49 +0200

    added error handling to try,catch in action function

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { useLoaderData, useNavigate, Form, redirect } from "react-router-dom";
+import {
+  useLoaderData,
+  useNavigate,
+  Form,
+  redirect,
+  useActionData,
+} from "react-router-dom";
 import { loginUser } from "../api";
 
 export function loader({ request }) {
@@ -10,17 +16,23 @@ export async function action({ request }) {
   const formData = await request.formData();
   const email = formData.get("email");
   const password = formData.get("password");
-  const data = await loginUser({ email, password });
-  localStorage.setItem("loggedin", true);
-  console.log(data); // Working -- logging token & user data
-  return redirect("/host"); // Not working
+
+  try {
+    const data = await loginUser({ email, password });
+    localStorage.setItem("loggedin", true);
+    console.log(data); // Working -- logging token & user data
+    return redirect("/host"); // Not working
+  } catch (err) {
+    return err.message;
+  }
 }
 
 export default function Login() {
   const [status, setStatus] = React.useState("idle");
-  const [error, setError] = React.useState(null);
+  // const [error, setError] = React.useState(null);
   const message = useLoaderData();
   const navigate = useNavigate();
+  const errorMessage = useActionData();
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -38,7 +50,7 @@ export default function Login() {
     <div className="login-container">
       <h1>Sign in to your account</h1>
       {message && <h3 className="red">{message}</h3>}
-      {error && <h3 className="red">{error.message}</h3>}
+      {errorMessage && <h3 className="red">{errorMessage}</h3>}
 
       <Form method="post" className="login-form" replace>
         <input name="email" type="email" placeholder="Email address" />
